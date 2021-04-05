211b88d4874be4610b8d6ea86736b6c44cbe2575
Cristian Camilo <tiancamilo59@gmail.com>
2021-04-05 02:11:35 -0500

    Change from visual studio

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -4,5 +4,5 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World from github!!!\n');
+    res.end('Hello World from visual studio!!!\n');
 }).listen(port);
